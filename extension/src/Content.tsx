2296e332928250d5f1cce60eaf020b3c8c3daaa0
Daniel Tao <danieltaox@gmail.com>
2023-12-10 12:42:22 -0500

    feat: show loading UI during inference

--- extension/src/Content.tsx
+++ extension/src/Content.tsx
@@ -5,12 +5,14 @@ function Content() {
   const [selectionText, setSelectionText] = useState<string>();
   const [selectionBoundingBox, setSelectionBoundingBox] = useState<DOMRect>();
   const [scrollTop, setScrollTop] = useState<number>(0);
+  const [opinion, setOpinion] = useState<any>();
 
   const popupRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleMessage = () => {
-      setIsOpen(true);
+    const handleMessage = (message: any) => {
+      if (message.type === 'open') setIsOpen(true);
+      if (message.type === 'opinion') setOpinion(message.data);
     }
     chrome.runtime.onMessage.addListener(handleMessage);
     return () => chrome.runtime.onMessage.removeListener(handleMessage);
@@ -37,6 +39,7 @@ function Content() {
     const handleClick = (e: MouseEvent) => {
       if (popupRef.current?.contains(e.target as Node) || e.target === popupRef.current) return;
       setIsOpen(false);
+      setOpinion(undefined);
     }
     document.addEventListener("click", handleClick);
     return () => document.removeEventListener("click", handleClick);
@@ -44,25 +47,32 @@ function Content() {
 
   const popupTop = selectionBoundingBox ? selectionBoundingBox.top + scrollTop : 0;
   const popupLeft = selectionBoundingBox ? Math.min(selectionBoundingBox.left + selectionBoundingBox.width + 10, window.innerWidth - 500) : 0;
+  const isLoading = isOpen && opinion === undefined;
   return (
-    isOpen && <div
+    isOpen ? <div
       ref={popupRef}
       css={{
         position: 'absolute',
         top: popupTop,
         left: popupLeft,
-        zIndex: Math.max,
+        zIndex: Number.MAX_VALUE,
         backgroundColor: 'white',
         color: 'black',
         border: '1px solid grey',
         padding: '25px',
         borderRadius: '5px',
         fontSize: '20px',
-        fontFamily: 'Sans-Serif'
+        fontFamily: 'Sans-Serif',
+        minWidth: '450px'
       }}
     >
-      {selectionText}
+      {
+        isLoading
+          ? 'Loading...'
+          : selectionText
+      }
     </div>
+   : null
   )
 }
 
--- extension/src/background.ts
+++ extension/src/background.ts
@@ -10,9 +10,13 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === "viewpoints-extension" && tab?.id) {
+    chrome.tabs.sendMessage(tab.id, { type: 'open' });
     // Send a message to the content script in the active tab
     const response = await ky.post(BACKEND_URL, { body: info.selectionText });
     const data = await response.json();
-    chrome.tabs.sendMessage(tab.id, data);
+    chrome.tabs.sendMessage(tab.id, {
+      type: "opinion",
+      data
+    });
   }
 })
