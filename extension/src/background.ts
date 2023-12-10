0f43d5c27b5e7abceb40f21491199b454eb54d34
Daniel Tao <danieltaox@gmail.com>
2023-12-10 11:20:25 -0500

    feat: install and use ky to query backend service

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,4 +1,6 @@
-export {};
+import ky from "ky";
+
+const BACKEND_URL = "http://localhost:3000";
 
 chrome.contextMenus.create({
   id: "viewpoints-extension",
@@ -8,9 +10,9 @@ chrome.contextMenus.create({
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === "viewpoints-extension" && tab?.id) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function () {
-      // Send a message to the content script in the active tab
-      chrome.tabs.sendMessage(tab.id!, { message: "myMessage" });
-    });
+    // Send a message to the content script in the active tab
+    const response = await ky.post(BACKEND_URL, { body: info.selectionText });
+    const data = await response.json();
+    chrome.tabs.sendMessage(tab.id, data);
   }
 })
