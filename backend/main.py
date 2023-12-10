475c47c78a8c18ead228c5d6e8d82f254ee83ed1
Daniel Tao <danieltaox@gmail.com>
2023-12-10 11:21:00 -0500

    feat: [WIP] scaffold opinion generator prompt

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-import psycopg, os, requests
+import psycopg, os, requests, together
+
+api_key = os.environ["LLM_API_KEY"]
 
 def get_embedding(text, model = "togethercomputer/GPT-NeoXT-Chat-Base-20B"):
   endpoint_url = "https://api.together.xyz/api/v1/embeddings"
-  api_key = os.environ["LLM_API_KEY"]
   
   headers = {
       "Authorization": f"Bearer {api_key}",
@@ -18,6 +19,33 @@ def get_embedding(text, model = "togethercomputer/GPT-NeoXT-Chat-Base-20B"):
   response = requests.post(endpoint_url, headers=headers, json=data)
   return response.json()["data"][0]["embedding"]
 
+def infer_opinion(query, context: list[str]):
+  new_line = "\n\n\n"
+  output = together.Complete.create(
+    prompt = f"""
+      You are a fair and impartial opinion generator. Using the listed source documents, give an opinion on the provided statement.
+
+      5 lines under the opinion, list the number corresponding to each source used to provide the opinion on a new line.
+      
+      Sources: {[
+        f"{i + 1}: {line}" in context for i, line in enumerate(context)
+      ].join(new_line)}
+
+      Statement: {query}
+
+      Opinion:
+    """, 
+    model = 'togethercomputer/StripedHyena-Nous-7B', 
+    max_tokens = 256,
+    temperature = 0.8,
+    top_k = 60,
+    top_p = 0.6,
+    repetition_penalty = 1.1,
+    stop = ['\n\n']
+  )
+
+  return output['output']['choices'][0]['text']
+
 app = FastAPI()
 
 @app.post("/")
@@ -28,5 +56,8 @@ def retrieve_nearest_viewpoints(query: str):
       embedding = get_embedding(query)
       cur.execute(f"SELECT * FROM items ORDER BY embedding <-> '{embedding}' LIMIT 5;")
 
-      # Return the top 5 results
-      return cur.fetchall()
+      items = cur.fetchall()
+      return {
+        "sources": items,
+        "opinion": infer_opinion(query, [item[1] for item in items]),
+      }
