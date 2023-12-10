8ba205e8d3689be0c5d429b99d87aad02ca92039
Daniel Tao <danieltaox@gmail.com>
2023-12-10 03:03:43 -0500

    feat: embed query plaintext from together.ai docs

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,22 @@
 from fastapi import FastAPI
-import psycopg
+import psycopg, os, requests
+
+def get_embedding(text, model = "togethercomputer/GPT-NeoXT-Chat-Base-20B"):
+  endpoint_url = "https://api.together.xyz/api/v1/embeddings"
+  api_key = os.environ["LLM_API_KEY"]
+  
+  headers = {
+      "Authorization": f"Bearer {api_key}",
+      "Content-Type": "application/json",
+  }
+  
+  data = {
+      "model": model,
+      "input": text[:512],
+  }
+  
+  response = requests.post(endpoint_url, headers=headers, json=data)
+  return response.json()["data"][0]["embedding"]
 
 app = FastAPI()
 
@@ -8,7 +25,7 @@ def retrieve_nearest_viewpoints(query: str):
   with psycopg.connect() as conn:
     with conn.cursor() as cur:
       # Get the embedding of the query
-      embedding = query
+      embedding = get_embedding(query)
       cur.execute(f"SELECT * FROM items ORDER BY embedding <-> '{embedding}' LIMIT 5;")
 
       # Return the top 5 results
