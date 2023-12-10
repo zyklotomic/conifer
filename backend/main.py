fc594b3689aa5663159cccc73da67ff6ed48996c
Ethan Kiang <chocopuff298@gmail.com>
2023-12-10 14:15:18 -0500

    ethan changes

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import psycopg, os, requests, together
 from pgvector.psycopg import register_vector
 import numpy as np
 
 api_key = os.environ["LLM_API_KEY"]
-DB_CONN = "host=localhost port=5432 user=postgres" \
+PORT = 5431
+DB_CONN = f"host=localhost port={PORT} user=postgres" \
     " password=example_password"
 together.api_key = api_key
-
-
-def get_embedding(text, model = "togethercomputer/GPT-NeoXT-Chat-Base-20B"):
+ 
+def get_embedding(text, model = "togethercomputer/bert-base-uncased"):
   endpoint_url = "https://api.together.xyz/api/v1/embeddings"
-  
+ 
   headers = {
       "Authorization": f"Bearer {api_key}",
       "Content-Type": "application/json",
@@ -33,9 +35,9 @@ def infer_opinion(query, context: list[str]):
 
       5 lines under the opinion, list the number corresponding to each source used to provide the opinion on a new line.
       
-      Sources: {[
-        f"{i + 1}: {line}" in context for i, line in enumerate(context)
-      ].join(new_line)}
+      Sources: {new_line.join(
+        f"{i + 1}: {line}" for i, line in enumerate(context)
+      )}
 
       Statement: {query}
 
@@ -54,20 +56,31 @@ def infer_opinion(query, context: list[str]):
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class OpinionQuery(BaseModel):
+    content_to_embed: str
+
 @app.post("/")
-def retrieve_nearest_viewpoints(content_to_embed: str):
+def retrieve_nearest_viewpoints(opinion_request: OpinionQuery):
   with psycopg.connect(DB_CONN) as conn:
     register_vector(conn)
     with conn.cursor() as cur:
       # Get the embedding of the query
-      embedding = get_embedding(content_to_embed)
+      embedding = get_embedding(opinion_request.content_to_embed)
       pg_query = \
       """
         SELECT
             news_vectors.uri, lead_paragraph, web_url,
-            1 - (embedding <=> %s) AS cos_sim
+            1 - (embedding <=> %s) AS cos_sim, headline
         FROM news_vectors
-        INNER JOIN news_data ON news_vectors.uri LIKE news_data.uri
+        LEFT JOIN news_data ON news_vectors.uri LIKE news_data.uri
         WHERE
             1 - (embedding <=> %s) > 0.70
         ORDER BY
@@ -80,17 +93,19 @@ def retrieve_nearest_viewpoints(content_to_embed: str):
       res = cur.fetchall()
       sources = []
       for r in res:
-        uri, lead_paragraph, web_url, cos_sim = r
+        uri, lead_paragraph, web_url, cos_sim, headline = r
         sources.append({
           'uri': uri,
           'lead_paragraph': lead_paragraph,
           'web_url': web_url,
-          'cos_sim': cos_sim
+          'cos_sim': cos_sim,
+          'headline': headline
         })
 
       lead_paragraphs =  [ s['lead_paragraph'] for s in sources ]
 
       return {
         "sources": sources,
-        "opinion": infer_opinion(content_to_embed, lead_paragraphs),
+        "opinion": infer_opinion(opinion_request.content_to_embed, lead_paragraphs) if
+          lead_paragraphs else "Unable to find sufficient sources to provide an opinion.",
       }
